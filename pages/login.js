61049e2ea0894a3f96ba1781b66fbe92c403335f
AbdiMD <abdi.zacky33@gmail.com>
2025-04-19 23:34:18 +0700

    small changes on login.js constructor

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,19 @@ const { expect } = require("@playwright/test");
 class LoginPage {
   constructor(page) {
     this.page = page;
+    this.usernameInput = page.getByRole('textbox', { name: 'Username' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.usernameErrorMessage = page.getByText('Required').first();
+    this.passwordErrorMessage = page.getByText('Required').first();
+    this.alert = page.getByRole('alert');
   }
 
   async login(base_url,username, password) {
     await this.page.goto(`${base_url}/web/index.php/auth/login`);
-    await this.page.getByRole('textbox', { name: 'Username' }).fill(username)
-    await this.page.getByRole('textbox', { name: 'Password' }).fill(password)
-    await this.page.getByRole('button', { name: 'Login' }).click();
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
   }
 }
 
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,11 +2,25 @@ const {test, page, expect} = require('@playwright/test');
 const {LoginPage} = require('../pages/login'); 
 require('dotenv').config();
 
-test('login page',async ({page})=>{
+
+test('Valid login',async ({page})=>{
     const loginPage = new LoginPage(page);
-    console.log(process.env.BASE_URL);
-    console.log(process.env.LOGIN_USERNAME);
-    console.log(process.env.LOGIN_PASSWORD);
     await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD);
     await expect(page.url()).toEqual('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
-})
+});
+
+test('Invalid login - Empty form',async ({page})=>{
+    const loginPage = new LoginPage(page);
+    await loginPage.login(process.env.BASE_URL, '', '');
+    await expect(loginPage.usernameErrorMessage).toBeVisible();
+    await expect(loginPage.passwordErrorMessage).toBeVisible();
+});
+
+test('Invalid login - Invalid credentials',async ({page})=>{
+    const loginPage = new LoginPage(page);
+    await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, 'invalidPassword');
+    await expect(loginPage.usernameErrorMessage).not.toBeVisible();
+    await expect(loginPage.passwordErrorMessage).not.toBeVisible();
+    await expect(loginPage.alert).toBeVisible();
+    await expect(loginPage.alert).toContainText('Invalid credentials');
+});
