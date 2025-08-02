a99a52d9c166cd9bdb93d4855b57dc129427908d
Zeya Firdaus Widyaka <zwidyaka91@gmail.com>
2025-08-02 15:48:32 +0700

    Add forgot password functioanlity and verify the URL

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,26 +1,31 @@
-const {test, page, expect} = require('@playwright/test');
-const {LoginPage} = require('../pages/login'); 
-require('dotenv').config();
+const { test, page, expect } = require("@playwright/test");
+const { LoginPage } = require("../pages/login");
+require("dotenv").config();
 
-
-test('Valid login',async ({page})=>{
-    const loginPage = new LoginPage(page);
-    await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD);
-    await expect(page.url()).toEqual('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+test("Valid login", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD);
+  await expect(page.url()).toEqual("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
 });
 
-test('Invalid login - Empty form',async ({page})=>{
-    const loginPage = new LoginPage(page);
-    await loginPage.login(process.env.BASE_URL, '', '');
-    await expect(loginPage.usernameErrorMessage).toBeVisible();
-    await expect(loginPage.passwordErrorMessage).toBeVisible();
+test("Invalid login - Empty form", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.BASE_URL, "", "");
+  await expect(loginPage.usernameErrorMessage).toBeVisible();
+  await expect(loginPage.passwordErrorMessage).toBeVisible();
 });
 
-test('Invalid login - Invalid credentials',async ({page})=>{
-    const loginPage = new LoginPage(page);
-    await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, 'invalidPassword');
-    await expect(loginPage.usernameErrorMessage).not.toBeVisible();
-    await expect(loginPage.passwordErrorMessage).not.toBeVisible();
-    await expect(loginPage.alert).toBeVisible();
-    await expect(loginPage.alert).toContainText('Invalid credentials');
+test("Invalid login - Invalid credentials", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.login(process.env.BASE_URL, process.env.LOGIN_USERNAME, "invalidPassword");
+  await expect(loginPage.usernameErrorMessage).not.toBeVisible();
+  await expect(loginPage.passwordErrorMessage).not.toBeVisible();
+  await expect(loginPage.alert).toBeVisible();
+  await expect(loginPage.alert).toContainText("Invalid credentials");
+});
+
+test("Forgot password link", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  await page.getByText("Forgot your password?", { exact: true }).click();
+  await expect(page.url()).toEqual("https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode");
 });
