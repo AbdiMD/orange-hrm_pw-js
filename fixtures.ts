f9c2947e3e53141cc593e2b3786aa2155d1b1c36
AbdiMD <abdi.zacky33@gmail.com>
2025-08-15 00:28:15 +0700

    adding usermanagement module

--- fixtures.ts
+++ fixtures.ts
@@ -1,9 +1,11 @@
 import { test as base } from '@playwright/test';
 import { LoginPage } from './pages/login';
+import { UserManagementPage } from './pages/userManagement';
 
 // Define the custom fixtures with a TypeScript interface.
 interface CustomFixtures {
   loginPage: LoginPage;
+  userManagementPage: UserManagementPage;
 }
 
 // Extend the test object with our custom fixtures.
@@ -11,6 +13,10 @@ export const test = base.extend<CustomFixtures>({
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await use(loginPage);
+  },
+  userManagementPage: async ({ page }, use) => {
+    const userManagementPage = new UserManagementPage(page);
+    await use(userManagementPage);
   },
 });
 
